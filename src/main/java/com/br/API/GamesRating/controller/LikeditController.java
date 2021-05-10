654e062940632b52fc8b48f70d7c673b66cfd2b5
Welbert Fernandes Costa <welbert.jsj@gmail.com>
2021-05-10 13:29:21 -0300

    criado service like

---
+++ src/main/java/com/br/API/GamesRating/controller/LikeditController.java
@@ -0,0 +1,36 @@
+package com.br.API.GamesRating.controller;
+
+import com.br.API.GamesRating.dto.NewLikeditDTO;
+import com.br.API.GamesRating.dto.UpdateLikeditDTO;
+import com.br.API.GamesRating.model.Likedit;
+import com.br.API.GamesRating.service.LikeditService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/like")
+public class LikeditController {
+
+    @Autowired
+    private LikeditService likeditService;
+
+    @PostMapping
+    public ResponseEntity<Likedit> insert(@Valid  @RequestBody NewLikeditDTO likeditDTO){
+        var likedit = likeditService.insert(likeditDTO);
+        var uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(likedit.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Likedit> update(@Valid @RequestBody UpdateLikeditDTO likeditDTO, @PathVariable Integer id){
+        var likedit = likeditService.update(id, likeditDTO);
+        return ResponseEntity.ok().body(likedit);
+    }
+}
