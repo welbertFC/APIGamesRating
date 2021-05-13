38d6d829d6897c8edee8ac432478a0f0b413d30a
Welbert Fernandes <welbert.jsj@gmail.com>
2021-05-12 22:42:17 -0300

    create conection

--- src/main/java/com/br/API/GamesRating/settings/TestSetting.java
+++
@@ -1,9 +0,0 @@
-package com.br.API.GamesRating.settings;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-@Configuration
-@Profile("test")
-public class TestSetting {
-}
