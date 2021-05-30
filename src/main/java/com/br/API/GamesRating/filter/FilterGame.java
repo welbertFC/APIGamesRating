ffcb12fbf520f12fdfb49100a70b454f3ea2cb7c
Welbert Fernandes <welbert.jsj@gmail.com>
2021-05-30 17:26:49 -0300

    create filter game

---
+++ src/main/java/com/br/API/GamesRating/filter/FilterGame.java
@@ -0,0 +1,11 @@
+package com.br.API.GamesRating.filter;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class FilterGame {
+
+    private String title;
+}
