9195300b294f0cbc94bda49b144ae0b1295666be
Welbert Fernandes <welbert.jsj@gmail.com>
2021-06-12 20:07:31 -0300

    send email

---
+++ src/main/java/com/br/API/GamesRating/service/email/EmailService.java
@@ -0,0 +1,12 @@
+package com.br.API.GamesRating.service.email;
+
+import com.br.API.GamesRating.model.UserClient;
+
+import javax.mail.internet.MimeMessage;
+
+public interface EmailService {
+
+  void sendOrderConfirmationHtmlEmail(UserClient userClient);
+
+  void sendHtmlEmail(MimeMessage msg);
+}
