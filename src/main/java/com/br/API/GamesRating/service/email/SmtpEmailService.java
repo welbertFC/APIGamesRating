2dcc41c9e7587245dca139cb5e2236b62fa4edf7
Welbert Fernandes <welbert.jsj@gmail.com>
2021-06-13 17:39:47 -0300

    teste email

--- src/main/java/com/br/API/GamesRating/service/email/SmtpEmailService.java
+++ src/main/java/com/br/API/GamesRating/service/email/SmtpEmailService.java
@@ -1,7 +1,5 @@
 package com.br.API.GamesRating.service.email;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 
@@ -9,14 +7,10 @@ import javax.mail.internet.MimeMessage;
 
 public class SmtpEmailService extends AbstractEmailService {
 
-  private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
-
   @Autowired private JavaMailSender javaMailSender;
 
   @Override
   public void sendHtmlEmail(MimeMessage msg) {
-    LOG.info("Enviando email...");
     javaMailSender.send(msg);
-    LOG.info("Email enviado");
   }
 }
